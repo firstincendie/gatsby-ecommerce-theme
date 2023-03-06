28c4f3470282bdcc2c3d3b5c2f5b3078e1941b72
firstincendie <127097544+firstincendie@users.noreply.github.com>
2023-03-06 11:18:39 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>💙 LETS GROW !</title>
+        <title>CONTRE LES INCENDIES !</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
